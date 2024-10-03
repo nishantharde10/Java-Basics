ebad1e9759613f639b9841942924ded99c634c49
nishantharde10 <nishantharde10@gmail.com>
2024-10-03 21:00:35 +0530

    Comments added

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Example input
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        // Calculate the average using the method
+        double average = solution.calculateAverage(numbers);
+        
+        // Output the average
+        System.out.println("The average is: " + average);
+    }
+}
---
+++ Pattern.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class Pattern {
+
+    public static void circleData(double radius){
+        double circumference = 2* Math.PI *radius;
+        double area = Math.PI*radius*radius;
+        System.out.println("cir si:" + circumference);
+        System.out.println("cir si:" + area);
+
+
+
+    
+       
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter radius:");
+
+        double radius = sc.nextDouble();
+
+        circleData(radius);
+
+
+
+
+       
+
+        
+
+        
+
+
+         
+
+
+
+    
+        
+
+
+
+
+
+    }
+    
+}
