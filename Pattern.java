7211046b0919d523e97784b87f8d19f9e0e05fa3
nishantharde10 <nishantharde10@gmail.com>
2024-10-03 21:32:55 +0530

    Resolved merge conflicts

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 public class Pattern {
 
+
+    //circumfernce and area of circle using method
+    
     public static void circleData(double radius){
         double circumference = 2* Math.PI *radius;
         double area = Math.PI*radius*radius;
         System.out.println("cir si:" + circumference);
         System.out.println("cir si:" + area);
-
-
-
-    
-       
     }
-
-
 
     public static void main(String[] args) {
 
