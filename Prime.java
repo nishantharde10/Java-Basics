0b28c99395c1e646710d575a660e44e1628c60ab
nishantharde10 <nishantharde10@gmail.com>
2024-10-04 15:50:48 +0530

    Prime or not using method

---
+++ Prime.java
@@ -0,0 +1,30 @@
+public class Prime {
+
+    public static boolean isPrime(int num){
+        if(num <=1){
+            return false;
+        }
+        for (int i=2; i<=Math.sqrt(num); i++){
+            if (num % i ==0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (isPrime(number)){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("not prime");
+        }
+        
+    }
+    
+}
